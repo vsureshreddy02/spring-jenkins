9928bec306f0abe10d74eaeb67c493acc94a1d25
vsureshreddy02 <vsuresh202@gmail.com>
2021-11-08 22:11:06 +0530

    Spring-Jenkins Demo Code

---
+++ src/main/java/springjenkins/HelloController.java
@@ -0,0 +1,16 @@
+package springjenkins;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@RequestMapping("/")  
+	public String hello()   
+	{  
+	return "Hello Jenkins";  
+	}  
+}
